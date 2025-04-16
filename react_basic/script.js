0a758089edf11a48ea565ce3399e988f261066ef
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-16 14:57:39 +0530

    feat: refactor script.js to create a structured container with username and password fields

--- react_basic/script.js
+++ react_basic/script.js
@@ -1,23 +1,25 @@
 
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 // const h1 = React.createElement("h1",{},"welcome to react")
-// const container = React.createElement("div",{id:"1"},
-//     [
-//         React.createElement("h1",{key:"heading"},"this is heading"),
-//         React.createElement("p",{className:"para1", key:"para1"},"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugit"),
-//         React.createElement("img",{key:"img",style:{width:"300px",borderRadius:"5px"},src:"https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}),
-//         React.createElement("form",{key:"form"},[
-//             React.createElement("div",{key:"forDiv",style:{padding:5,}},[
-//                 React.createElement("label",{key:"labelUsername",htmlFor:"username"},"username:"),
-//                 React.createElement("input",{type:"Text",key:"username", id:"username"}),
-//             ]),
-//             React.createElement("div",{key:"formDiv2",style:{padding:5,}},[
-//                 React.createElement("label",{key:"labelPasswod",htmlFor:"password"},"password:"),
-//                 React.createElement("input",{key:"password",type:"password",id:"password"})
-//             ])
-//         ])
-//     ]
-// )
+const user = "vikram"
+const container = React.createElement("div",{id:"1",className:"container"},
+    [
+        React.createElement("h1",{key:"heading"},"this is heading"),
+        React.createElement("p",{className:"para1", key:"para1"},"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugit"),
+        React.createElement("img",{key:"img",style:{width:"300px",borderRadius:"5px"},src:"https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}),
+        React.createElement("form",{key:"form"},[
+            React.createElement("div",{key:"forDiv",style:{padding:5,}},[
+                React.createElement("label",{key:"labelUsername",htmlFor:"username"},"username:"),
+                React.createElement("input",{type:"Text",key:"username", id:"username"}),
+            ]),
+            React.createElement("div",{key:"formDiv2",style:{padding:5,}},[
+                React.createElement("label",{key:"labelPasswod",htmlFor:"password"},"password:"),
+                React.createElement("input",{key:"password",type:"password",id:"password"})
+            ])
+        ]),
+        React.createElement("div",{key:"anotherDiv"},`hello,${user}`)
+    ]
+)
 // const container =  <div id="1">
 // <h1>this is heading</h1>
 // <p className="para1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugit</p>
@@ -37,17 +39,17 @@ const root = ReactDOM.createRoot(document.querySelector("#root"))
 // </form>
 // </div>
 
-const name = "vikram"
-const h2 = <h3>hello {name}</h3>
-const form = <form>
-    <div className="username">
-    <label htmlFor="username">username:</label>
-    <input id="username" type="text"  />
+// const name = "vikram"
+// const h2 = <h3>hello {name}</h3>
+// const form = <form>
+//     <div className="username">
+//     <label htmlFor="username">username:</label>
+//     <input id="username" type="text"  />
 
-    </div>
-    <div className="password">
-        <label htmlFor="password">password:</label>
-        <input for="password" type="password" />
-    </div>
-</form>
-root.render(form)
+//     </div>
+//     <div className="password">
+//         <label htmlFor="password">password:</label>
+//         <input id="password" type="password" />
+//     </div>
+// </form>
+root.render(container)
