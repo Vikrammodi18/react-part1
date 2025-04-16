71d69504417ae6500fde5c4c432761312b05090c
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-17 03:10:11 +0530

    feat: refactor App component to simplify counter logic and update UI elements

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,32 +1,21 @@
+import { useState } from "react"
 
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-
-  let [counter,setCounter]=useState(0)
-  const addValue = ()=>{
-   if(counter < 20){
-     setCounter(counter=>counter+1)
-
-   }
-    
+function App(){
+  // let counter = 13
+  let [counter,setCounter] = useState(15)
+  function increaseValue(){
+    setCounter(counter=>counter+1)
   }
-  const decreaseValue = ()=>{
-    if(counter>0){
-      setCounter(counter => counter -1)
-    }
+  function decreaseValue(){
+    setCounter(counter=> counter-1)
   }
-
   return (
     <>
-     <h1>chai ur code</h1>
-     <h3>counter value:{counter}</h3>
-     <button onClick={addValue}>increase value</button>
-     <button onClick={decreaseValue}>decrease value</button>
-     <p>you clicked :{counter} time</p>
-     <h1>counter: {counter}</h1>
-     <a href="https://react.dev" target='_blank'>click to react.dev</a>
+    <h1>hellow</h1>
+    <h3>counter:{counter}</h3>
+    <button onClick={increaseValue}>increase</button>
+    <button onClick={decreaseValue}>decrease</button>
+    <footer>{counter}</footer>
     </>
   )
 }
