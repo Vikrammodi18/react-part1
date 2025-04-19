06c9b41ae74be6c92b9725dcca279e18b95226b4
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-19 20:52:37 +0530

    feat: add Login component with FontAwesome icons and NavBar integration

--- multipleElement/src/main.jsx
+++ multipleElement/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './loginSignup/Login.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
-    <App />
+   <Login />
+    {/* <App /> */}
   </StrictMode>,
 )
--- multipleElement/src/navbar/NavBar.jsx
+++ multipleElement/src/navbar/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar (){
     return(
         <>
         <nav className="w-screen flex justify-center px-5 py-3">
-            <nav className="bg-gray-800 w-4xl text-white flex flex-wrap my-4 p-5 rounded-xl justify-between">
+            <nav className="bg-gray-500 w-4xl text-white flex flex-wrap my-4 p-5 rounded-xl justify-between">
             <div><img className="w-5" src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="" /></div>
             <div>
                 <ul className="flex flex-wrap px-2 py-1  gap-x-3">
