3c45ab8f0b26ad9e26edd87df70214938fdbee30
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-19 15:26:44 +0530

    feat: add NavBar component and integrate it into App layout

--- multipleElement/src/App.jsx
+++ multipleElement/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import Cards from './cardComponent/Card'
 import './App.css'
-
+import NavBar from './navbar/NavBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <div>
+     <NavBar />
      <Cards />
+
+    </div>
      
    
     </>
