bf4eb2b4b694422cf69d7c2b95b381a9c5f68534
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-20 12:00:43 +0530

    fix: add transition duration to search button for smoother hover effect

--- multipleElement/src/search/Search.jsx
+++ multipleElement/src/search/Search.jsx
@@ -7,7 +7,7 @@ function Search (){
         <div>
             <div className="border-solid border-1 inline-block  rounded-xl overflow-hidden ">
             <input type="text" className="outline-none w-m p-2"  placeholder="search for products" />
-            <input type="submit" className="py-2 px-2 bg-gray-400 hover:bg-gray-300 hover:text-gray-900" value="search" />
+            <input type="submit" className="py-2 px-2 bg-gray-400 duration-200 hover:bg-gray-300 hover:text-gray-900" value="search" />
 
             </div>
         </div>
