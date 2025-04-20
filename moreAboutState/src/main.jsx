8defaaf0508506fb3c49261a22feb74463c8d3f6
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-20 17:46:31 +0530

    feat: Initialize moreAboutState project with Vite and React setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Created main application structure including App.jsx, main.jsx, and styles.
    - Included Vite configuration for React plugin.
    - Added SVG assets for React and Vite logos.
    - Removed unused files and components from multipleElement project.

--- moreAboutState/src/main.jsx
+++ moreAboutState/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './loginSignup/Login.jsx'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Search from './search/Search.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   {/* <Login /> */}
-    {/* <App /> */}
-    <Search />
+    <App />
   </StrictMode>,
 )
